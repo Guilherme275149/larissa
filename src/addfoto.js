9a8e5871e95384ba508fbefa1c369130ee113885
guilherme <78052265+Guilherme275149@users.noreply.github.com>
2021-03-24 16:59:22 -0300

    Add files via upload

---
+++ src/addfoto.js
@@ -0,0 +1,11 @@
+const addfoto = () => { 
+	return `
+	
+	*ADICIONE FOTOS DE BANCO DE DADOS*
+	
+	FOTOS DE SUCESSO SALVADAS NOS DADOS!
+	
+
+Obrigado !`
+}
+exports.addfoto = addfoto
---
+++ src/daftarvip.js
@@ -0,0 +1,21 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DO PREMIUM:*
+
+-1. 10 > Acessar recursos premium
+-2. 40 > Recursos vip + base do bot editável!
+
+*SE QUER TER PREMIUM:*
+
+*CHAME O DARK:*
+
+_wa.me/5522996215481 ou digite *${prefix}owner*_
+
+*OU CHAME ELE NO GRUPO*
+
+*GRUPO DO DARK:*
+
+_https://chat.whatsapp.com/K6X6sVsfkCp1KZdIIuU4Kk_ `
+}
+exports.daftarvip = daftarvip
